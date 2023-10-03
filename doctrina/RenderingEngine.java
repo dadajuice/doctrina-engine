7544f37438e76052d45a1c3b6f8fcef63cb20dd2
David Tucker <davidt2003@msn.com>
2023-10-03 14:22:50 -0400

    feat: remove menu bar

--- doctrina/RenderingEngine.java
+++ doctrina/RenderingEngine.java
@@ -58,10 +58,10 @@ public class RenderingEngine {
         frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
-        frame.setTitle("My Bouncing Balls");
+        frame.setTitle("Doctrina Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setState(JFrame.NORMAL);
-        //frame.setUndecorated(true);
+        frame.setUndecorated(true);
     }
 
     private RenderingHints buildRenderingHints() {
