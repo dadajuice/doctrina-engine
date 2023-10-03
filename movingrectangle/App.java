5cf41c1b14528cf403c070345c636a5283e99ae9
David Tucker <davidt2003@msn.com>
2023-10-03 13:14:54 -0400

    feat: amazing new game

---
+++ movingrectangle/App.java
@@ -0,0 +1,8 @@
+package movingrectangle;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ movingrectangle/MovingRectangleGame.java
@@ -0,0 +1,22 @@
+package movingrectangle;
+
+import doctrina.Canvas;
+import doctrina.Game;
+
+public class MovingRectangleGame extends Game {
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void update() {
+
+    }
+
+    @Override
+    protected void draw(Canvas canvas) {
+
+    }
+}
