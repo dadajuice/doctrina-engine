a0ccb10bacdcc4ee8eaa9c57a2ae51f7c72e40b8
David Tucker <davidt2003@msn.com>
2023-10-16 10:35:03 -0400

    feat: prepare for collision

--- doctrina/MovableEntity.java
+++ doctrina/MovableEntity.java
@@ -1,5 +1,7 @@
 package doctrina;
 
+import java.awt.*;
+
 public abstract class MovableEntity extends StaticEntity {
 
     private int speed = 1;
@@ -33,6 +35,16 @@ public abstract class MovableEntity extends StaticEntity {
         move(Direction.RIGHT);
     }
 
+    public Rectangle getHitBox() {
+        switch (direction) {
+            case UP: return getUpperHitBox();
+            case DOWN: return getLowerHitBox();
+            case LEFT: return getLeftHitBox();
+            case RIGHT: return getRightHitBox();
+        }
+        return getBounds();
+    }
+
     public int getSpeed() {
         return speed;
     }
@@ -47,5 +59,35 @@ public abstract class MovableEntity extends StaticEntity {
 
     public void setDirection(Direction direction) {
         this.direction = direction;
+    }
+
+    public boolean hitBoxIntersectWith(StaticEntity other) {
+        if (other == null) {
+            return false;
+        }
+        return getHitBox().intersects(other.getBounds());
+    }
+
+    public void drawHitBox(Canvas canvas) {
+        Rectangle rectangle = getHitBox();
+        Color color = new Color(255, 0, 0, 200);
+        canvas.drawRectangle(rectangle.x, rectangle.y,
+                rectangle.width, rectangle.height, color);
+    }
+
+    private Rectangle getUpperHitBox() {
+        return new Rectangle(x, y - speed, width, speed);
+    }
+
+    private Rectangle getLowerHitBox() {
+        return new Rectangle(x, y + height, width, speed);
+    }
+
+    private Rectangle getLeftHitBox() {
+        return new Rectangle(x - speed, y, speed, height);
+    }
+
+    private Rectangle getRightHitBox() {
+        return new Rectangle(x + width, y, speed, height);
     }
 }
