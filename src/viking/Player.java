fb30441895fcc62a4fb5c7de3218a3ff5b30510b
David Tucker <davidt2003@msn.com>
2023-10-17 14:25:36 -0400

    feat: implement player spritesheet

--- src/viking/Player.java
+++ src/viking/Player.java
@@ -4,14 +4,26 @@ import doctrina.Canvas;
 import doctrina.ControllableEntity;
 import doctrina.MovementController;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends ControllableEntity {
+
+    private static final String SPRITE_PATH = "images/player.png";
+
+    private BufferedImage spriteSheet;
+    private Image[] rightFrames;
+    private Image[] leftFrames;
+    private Image[] upFrames;
+    private Image[] downFrames;
 
     public Player(MovementController controller) {
         super(controller);
         setDimension(32, 32);
         setSpeed(3);
+        load();
     }
 
     @Override
@@ -22,6 +34,41 @@ public class Player extends ControllableEntity {
 
     @Override
     public void draw(Canvas canvas) {
-        canvas.drawRectangle(this, Color.RED);
+        canvas.drawImage(downFrames[1], x, y);
+    }
+
+    private void load() {
+        loadSpriteSheet();
+        loadAnimationFrames();
+    }
+
+    private void loadSpriteSheet() {
+        try {
+            spriteSheet = ImageIO.read(this.getClass().getClassLoader().getResourceAsStream(SPRITE_PATH));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void loadAnimationFrames() {
+        downFrames = new Image[3];
+        downFrames[0] = spriteSheet.getSubimage(0, 128, width, height);
+        downFrames[1] = spriteSheet.getSubimage(32, 128, width, height);
+        downFrames[2] = spriteSheet.getSubimage(64, 128, width, height);
+
+        leftFrames = new Image[3];
+        leftFrames[0] = spriteSheet.getSubimage(0, 160, width, height);
+        leftFrames[1] = spriteSheet.getSubimage(32, 160, width, height);
+        leftFrames[2] = spriteSheet.getSubimage(64, 160, width, height);
+
+        rightFrames = new Image[3];
+        rightFrames[0] = spriteSheet.getSubimage(0, 192, width, height);
+        rightFrames[1] = spriteSheet.getSubimage(32, 192, width, height);
+        rightFrames[2] = spriteSheet.getSubimage(64, 192, width, height);
+
+        upFrames = new Image[3];
+        upFrames[0] = spriteSheet.getSubimage(0, 224, width, height);
+        upFrames[1] = spriteSheet.getSubimage(32, 224, width, height);
+        upFrames[2] = spriteSheet.getSubimage(64, 224, width, height);
     }
 }
