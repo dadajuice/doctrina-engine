2f197d287978067e5258dabdbe55a6cea20b8c7a
David Tucker <davidt2003@msn.com>
2023-10-17 14:55:40 -0400

    feat: implement tree layers

---
+++ src/viking/Tree.java
@@ -0,0 +1,31 @@
+package viking;
+
+import doctrina.Canvas;
+import doctrina.StaticEntity;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+
+public class Tree extends StaticEntity {
+
+    private static final String SPRITE_PATH = "images/tree.png";
+    private Image image;
+
+    public Tree() {
+        load();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawImage(image, x, y);
+    }
+
+    private void load() {
+        try {
+            image = ImageIO.read(this.getClass().getClassLoader().getResourceAsStream(SPRITE_PATH));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- src/viking/VikingGame.java
+++ src/viking/VikingGame.java
@@ -8,6 +8,7 @@ public class VikingGame extends Game {
     private Player player;
     private GamePad gamePad;
     private World world;
+    private Tree tree;
 
     @Override
     protected void initialize() {
@@ -16,6 +17,8 @@ public class VikingGame extends Game {
         player.teleport(200, 200);
         world = new World();
         world.load();
+        tree = new Tree();
+        tree.teleport(300, 350);
     }
 
     @Override
@@ -29,6 +32,13 @@ public class VikingGame extends Game {
     @Override
     protected void draw(Canvas canvas) {
         world.draw(canvas);
-        player.draw(canvas);
+        // 80 (tree height) - 28 (max for layer switch)
+        if (player.getY() < tree.getY() + 52) {
+            player.draw(canvas);
+            tree.draw(canvas);
+        } else {
+            tree.draw(canvas);
+            player.draw(canvas);
+        }
     }
 }
