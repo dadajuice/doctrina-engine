f64b3ed6c4bc30d62f17620f582e407e4aa9504f
David Tucker <davidt2003@msn.com>
2023-10-23 08:37:05 -0400

    feat: implement music

--- src/viking/VikingGame.java
+++ src/viking/VikingGame.java
@@ -24,6 +24,17 @@ public class VikingGame extends Game {
         world = new World();
         world.load();
         tree = new Tree(300, 350);
+
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream stream = AudioSystem.getAudioInputStream(
+                    this.getClass().getClassLoader().getResourceAsStream("audios/music.wav"));
+            clip.open(stream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
